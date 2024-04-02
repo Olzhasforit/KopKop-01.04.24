2775055024c1e21b48ddfceecf01c8c93f807a91
Olzhas <olzhas11223344@gmail.com>
2024-04-03 02:30:34 +0600

    settings

--- myproject/settings.py
+++ myproject/settings.py
@@ -92,6 +92,7 @@ STATIC_URL = '/static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 USE_X_FORWARDED_HOST = True
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 
 ALLOWED_HOSTS = ['51.20.138.197', 'localhost', '127.0.0.1']
 
