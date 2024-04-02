92988eac4fa6126f6130c7f7c62b39ef80081836
Olzhas <olzhas11223344@gmail.com>
2024-04-03 02:54:29 +0600

    settings

--- myproject/settings.py
+++ myproject/settings.py
@@ -91,7 +91,7 @@ STATIC_URL = '/static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-ALLOWED_HOSTS = ['51.20.138.197', 'localhost', '127.0.0.1', '51.20.138.197:8000']
+ALLOWED_HOSTS = ['51.20.138.197:8000', 'localhost', '127.0.0.1']
 
 
 
